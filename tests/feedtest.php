29a4be32f10a676b6c347dff348b510fcc5095ee
Jacob Ian Matthews <jacob@jacobian.com.au>
2018-06-25 00:48:47 +0800

    Uses implode() now

--- tests/feedtest.php
+++ tests/feedtest.php
@@ -8,12 +8,11 @@
 	$database = array(
 			"username"=>"root",
 			"password"=>"",
-			"host"=>"instagramfeed.local",
+			"host"=>"instagram-feed.local",
 			"database"=>"instagram"
 	);
 
-	$feed = new instagramFeed($database, 10);
-	echo "<pre>";
-	print_r($feed->feed());
+	$feed = new instagramFeed($database, 10, 'home');
+	echo implode("\n", $feed->feed());
 
 ?>
